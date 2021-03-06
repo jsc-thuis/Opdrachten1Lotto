0d1550902c0caf934d2d687007c07b84d5ed9a51
Jos <jozef.casteels@thomasmore.be>
2021-03-06 12:08:43 +0100

    Opdrachten 1 LOTTO OPGAVE

---
+++ src/Lotto.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.Arrays;
+import java.util.List;
+
+public class Lotto {
+    private Integer[] lottogetallen;
+    private int bonusgetal;
+    private Random random;
+
+    public Lotto() {
+        lottogetallen = new Integer[6];
+        bonusgetal = 0;
+        random = new Random();
+    }
+
+    public void trekking() {
+        // array lottogetallen en bonusgetal trekken en afdrukken met for-lus
+    }
+
+    public void sorteer() {
+        // array lottogetallen sorteren en afdrukken samen met bonusgetal
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        Lotto lotto = new Lotto();
+        System.out.println("We testen onze lotto-trekking...");
+        lotto.trekking();
+        lotto.sorteer();
+    }
+}
